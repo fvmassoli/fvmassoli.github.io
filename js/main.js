8bf650e06bfd825037d2e7b43da40076d0a3648f
Fabio Valerio <fabiovaleriomassoli@gmail.com>
2020-06-30 11:47:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Fabio Valerio Massoli.", "Scientist.", "Pianist.", "Dreamer.", "Consultant."],
       typeSpeed: 100,
       loop: true,
     });
